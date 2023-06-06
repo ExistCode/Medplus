c7e5c6083d5bb97ec62edc0415b293aa861cb5a3
Hans Andreanto <andreantohanss@gmail.com>
2023-06-06 15:50:39 +0800

    Changed-menubutton-to-combo-box

--- src/main/java/medplus/controllers/Addpatientcontroller.java
+++ src/main/java/medplus/controllers/Addpatientcontroller.java
@@ -2,20 +2,39 @@ package medplus.controllers;
 
 import java.io.IOException;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-
+import javafx.scene.control.ComboBox;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import medplus.App;
 
 public class Addpatientcontroller {
+    ObservableList<String> bloodTypeOptions = FXCollections.observableArrayList(
+            "A-",
+            "A+",
+            "B-",
+            "B+",
+            "O-",
+            "O+",
+            "AB-",
+            "AB+");
 
+    ObservableList<String> genderOptions = FXCollections.observableArrayList(
+            "Male",
+            "Female");
+
+    @FXML
+    private ComboBox<String> Gender;
     @FXML
     private Pane addPatientButton;
 
     @FXML
     private ImageView backButton;
+    @FXML
+    private ComboBox<String> bloodType;
 
     @FXML
     void addPatient(MouseEvent event) throws IOException {
@@ -26,6 +45,16 @@ public class Addpatientcontroller {
     @FXML
     void backToPatients(MouseEvent event) throws IOException {
         App.setRoot("patients");
+
+    }
+
+    /**
+     * 
+     */
+    @FXML
+    public void initialize() {
+        bloodType.setItems(bloodTypeOptions);
+        Gender.setItems(genderOptions);
 
     }
 
