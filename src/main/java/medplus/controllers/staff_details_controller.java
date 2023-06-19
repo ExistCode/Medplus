eab97c95a100e77993b209c7068ef5e615f80557
richard <richardcharlie659@gmail.com>
2023-06-19 15:38:45 +0800

    staff details controller delete staff error

--- src/main/java/medplus/controllers/staff_details_controller.java
+++ src/main/java/medplus/controllers/staff_details_controller.java
@@ -1,18 +1,31 @@
 package medplus.controllers;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import medplus.App;
 import medplus.data.PatientData;
 import medplus.data.StaffData;
+import medplus.tableModels.MedicalHistoryTableDataModel;
 
 public class staff_details_controller {
+    @FXML
+    private Pane deleteStaffButton;
+    @FXML
+    private Pane updateButton;
+    @FXML
+    private Pane editStaffButton;
     @FXML
     private Pane analysisButton;
     @FXML
@@ -50,6 +63,9 @@ public class staff_details_controller {
 
     @FXML
     private Pane treatmentButton;
+
+    @FXML
+    private TableView<MedicalHistoryTableDataModel> patientMedicalHistoryTable;
 
     @FXML
     void changedToDashboard(MouseEvent event) throws IOException {
@@ -91,6 +107,31 @@ public class staff_details_controller {
     void changedToAddMedicalHistory(MouseEvent event) throws IOException {
         App.setRoot("add_medical_history_screen_staff");
     }
+
+    @FXML
+    void deleteStaff(MouseEvent event) throws IOException {
+        StaffData.deleteStaffById(StaffData.initStaffData.getStaffId());
+        App.setRoot("staff_all_home_screen");
+    }
+
+    @FXML
+    void editStaffDetails(MouseEvent event) throws IOException {
+        // PatientData.initPatientData.setPatientId(PatientData.initPatientData.getPatientId());
+        // PatientData.initPatientData.setPatientName(PatientData.initPatientData.getPatientName());
+        // PatientData.initPatientData.setPatientNationalId("6789012345");
+        // PatientData.initPatientData.setPatientGender(PatientData.initPatientData.getPatientGender());
+        // PatientData.initPatientData.setPatientDateOfBirth(PatientData.initPatientData.getPatientDateOfBirth());
+        // PatientData.initPatientData.setPatientAge(PatientData.initPatientData.getPatientAge());
+        // PatientData.initPatientData.setPatientHeight(PatientData.initPatientData.getPatientHeight());
+        // PatientData.initPatientData.setPatientWeight(PatientData.initPatientData.getPatientWeight());
+        // PatientData.initPatientData.setPatientBloodType(PatientData.initPatientData.getPatientBloodType());
+        // PatientData.initPatientData.setPatientAddress("Miami");
+        // PatientData.initPatientData.setPatientContactNumber("60238343422");
+
+        App.setRoot("update_staff_screen");
+
+    }
+    
 
     @FXML
     public void initialize() {
